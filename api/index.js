aa1c0752744e566d65c97aaa1714c39babf3a2b8
slvarun <varun.merugu@yahoo.in>
2023-11-29 22:33:10 +0530

    c-2

--- api/index.js
+++ api/index.js
@@ -1,78 +1,83 @@
-import express from "express";
-import dotenv from "dotenv";
-import mongoose from "mongoose"
-import monumentsRoute from "./routes/monuments.js"
-import userRoute from "./routes/users.js"
-import authRoute from "./routes/auth.js"
-import bookingRoute from "./routes/bookingorder.js"
-import AdminRoute from "./routes/admin.js"
-import cookieParser from "cookie-parser"
-
-
-const app = express();
-dotenv.config();
-
-
-
-mongoose.set('strictQuery', true);
-const connect = async () => {
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({ path: __dirname + '/.env' });
+  }
+  
+  const express = require('express');
+  const mongoose = require('mongoose');
+  const cookieParser = require('cookie-parser');
+  const monumentsRoute = require('./routes/monuments.js');
+  const userRoute = require('./routes/users.js');
+  const authRoute = require('./routes/auth.js');
+  const bookingRoute = require('./routes/bookingorder.js');
+  const AdminRoute = require('./routes/admin.js');
+  const path = require('path');
+  
+  const app = express();
+  app.use(cookieParser());
+  app.use(express.json());
+  
+  const port = process.env.PORT || 5000;
+  
+  mongoose.set('strictQuery', true);
+  
+  const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-        })
-        console.log(("connected to db"))
+      await mongoose.connect(process.env.MONGO, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+      });
+      console.log('Connected to db');
     } catch (error) {
-        console.log("Not connected")
+      console.log('Not connected');
     }
-}
-mongoose.connection.on("disconnected", () => {
-    console.log("mongoDB disconnected")
-})
-
-
-
-
-app.use(cookieParser())
-//  default middlewares
-app.use(express.json())
-
-
-
-
-
-app.use("/api/monuments", monumentsRoute);
-app.use("/api/users", userRoute)
-//  using other files as library
-app.use("/api/auth", authRoute)
-app.use("/api/bookingorder", bookingRoute)
-app.use("/api/admin", AdminRoute)
-
-
-
-
-
-app.use((err, req, res, next) => {
-    const errorStatus = err.status || 500
-    const errormsg = err.message || "Something went wrong"
-
+  };
+  
+  mongoose.connection.on('disconnected', () => {
+    console.log('MongoDB disconnected');
+  });
+  
+  // Routes
+  app.use('/api/monuments', monumentsRoute);
+  app.use('/api/users', userRoute);
+  app.use('/api/auth', authRoute);
+  app.use('/api/bookingorder', bookingRoute);
+  app.use('/api/admin', AdminRoute);
+  
+  // Error handling middleware
+  app.use((err, req, res, next) => {
+    const errorStatus = err.status || 500;
+    const errorMsg = err.message || 'Something went wrong';
+  
     return res.status(errorStatus).json({
-        success: false,
-        status: errorStatus,
-        message: errormsg,
-        stack: err.stack,
+      success: false,
+      status: errorStatus,
+      message: errorMsg,
+      stack: err.stack,
+    });
+  });
+  
+  mongoose
+    .connect(process.env.mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      poolSize: 1,
     })
-})
-
-
-
-
-
-
-
-app.listen(process.env.PORT || 5000, () => {
-    connect();
-    console.log("Listening to 5000");
-})
+    .then(() => {
+      app.listen(port, () => {
+        connect();
+        console.log(`Listening to ${port}`);
+      });
+    });
+  
+  // Serve static files (build of your frontend)
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../my_app', 'build')));
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../my_app', 'build', 'index.html'));
+    });
+  }
+  
